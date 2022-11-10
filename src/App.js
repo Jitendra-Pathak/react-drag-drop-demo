234da1ebae50ca6b7d57247435e86fda1dff97fd
Jitendra Pathak <jitendra.pathak@infoedge.com>
2022-11-10 12:52:05 +0530

    demo setup .......................

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import SimpleListDragDrop from "simplelistdragdrop";
+import ListDragDrop from "listdragdrop";
 import { SIDEBAR_DATA, DRAGLIST_DATA, TABS_DATA } from './constants';
 import './App.css';
 
@@ -25,68 +25,81 @@ function App() {
     console.log('sidebar onDragEnd data', data);
   };
 
-  
+
+  const getHeader = () => {
+    return (
+      <div className='headerContainer'>
+        <h3>ListDragDrop</h3>
+        <div className='otherLinks'>
+          <a href='https://github.com/Jitendra-Pathak/listdragdrop/blob/main/README.md' target="_blank">Documentation</a>
+          <a href='https://github.com/naukri-engineering' target="_blank">Other Plugins</a>
+        </div>
+      </div>
+    );
+  };
 
   return (
-    <div className='container'>
-      <div className='sidebar'>
-        <SimpleListDragDrop
-          containerId='sidebarDropArea'
-          containerClass='colDragCustomClass'
-          options={SIDEBAR_DATA}
-          onDragStartCallback={onDragStart}
-          onDragOverCallback={onDragOver}
-          onDragEndCallback={onDragEnd}
-        />
-      </div>
-      
-
-      <div className='content'>
-        <h1 className="demoPageTitle">Drag and Drop Example </h1>
-
-        <div className='exptext'>
-          <b>Example 1 (Sidebar view): </b>
-          <span>This is default example with default UI</span>
-        </div>
-
-        <div className='exptext'>
-          <b>Example 2: </b>
-          <span>
-            This is with custom template for custom UI using 
-            <b>"customTemplate"</b> prop with a Delete action.
-          </span>
-          <p>Also we made option disable from drag and drop using <b>"draggable"</b> key in data.</p>
-        </div>
-        <div className='wrapper vertical'>
-          <SimpleListDragDrop
-            containerId='verticalDropArea'
-            containerClass='customClass'
-            options={DRAGLIST_DATA}
-            customTemplate={getDragRowAction}
-            itemClass="customListItem"
+    <div className='wrapper'>
+      {getHeader()}
+      <div className='container'>
+        <div className='sidebar'>
+          <p className='label'>Example 1 </p>
+          <ListDragDrop
+            containerId='sidebarDropArea'
+            containerClass='colDragCustomClass'
+            options={SIDEBAR_DATA}
             onDragStartCallback={onDragStart}
             onDragOverCallback={onDragOver}
             onDragEndCallback={onDragEnd}
           />
         </div>
 
-        <div className='exptext'>
-          <b>Example 3: </b>
-          <span>Here we override Style using custom class with default component UI.</span>
-        </div>
+        <div className='content'>
 
-        <div className='wrapper horizontal'>
-          <SimpleListDragDrop
-            containerId='horizontalDropArea'
-            containerClass='customClass'
-            options={TABS_DATA}
-            itemClass="customListItem"
-            onDragStartCallback={onDragStart}
-            onDragOverCallback={onDragOver}
-            onDragEndCallback={onDragEnd}
-          />
-        </div>
+          <div className='exptext'>
+            <b>Example 1: Left sidebar view </b>
+            <p>This is default example with default UI</p>
+          </div>
 
+          <div className='exptext'>
+            <b>Example 2: </b>
+            <p>
+              This is with custom template for custom UI using
+              <b>"customTemplate"</b> prop with a Delete action.
+            </p>
+            <p>Also we made option disable from drag and drop using <b>"draggable"</b> key in data.</p>
+          </div>
+          <div className='wrapper vertical'>
+            <ListDragDrop
+              containerId='verticalDropArea'
+              containerClass='customClass'
+              options={DRAGLIST_DATA}
+              customTemplate={getDragRowAction}
+              itemClass="customListItem"
+              onDragStartCallback={onDragStart}
+              onDragOverCallback={onDragOver}
+              onDragEndCallback={onDragEnd}
+            />
+          </div>
+
+          <div className='exptext'>
+            <b>Example 3: </b>
+            <p>Here we override Style using custom class with default component UI.</p>
+          </div>
+
+          <div className='wrapper horizontal'>
+            <ListDragDrop
+              containerId='horizontalDropArea'
+              containerClass='customClass'
+              options={TABS_DATA}
+              itemClass="customListItem"
+              onDragStartCallback={onDragStart}
+              onDragOverCallback={onDragOver}
+              onDragEndCallback={onDragEnd}
+            />
+          </div>
+
+        </div>
       </div>
     </div>
   );
